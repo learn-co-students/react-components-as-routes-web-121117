6b7b82885870e77547140ffd01b49197e317cde3
markbello <mcbello51286@gmail.com>
2018-02-27 14:21:55 -0500

    Done.

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class About extends React.Component {
+
+  state = {}
+
+  render() {
+    return (
+      <div>
+        <h1>This is my about component!</h1>
+      </div>
+    );
+  }
+}
+
+export default About
--- src/index.js
+++ src/index.js
@@ -1,15 +1,93 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Messages from './Messages'
 
-const Home = () => {
-  return (
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const SignUp = () =>
+  <h1>Sorry, you're not cool enough</h1>;
+
+
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  <NavLink
+    to='/signup'
+    exact
+    style={link}
+    activeStyle={{
+      background: 'darkblue'
+    }}
+    >Signup Yo
+  </NavLink>
+  <NavLink
+    to='/signup/pl0x'
+    exact
+    style={link}
+    activeStyle={{
+      background: 'darkblue'
+    }}
+    >Signup pl0x??
+  </NavLink>
+  <NavLink
+    to='/messages'
+    exact
+    style={link}
+    activeStyle={{
+      background: 'darkblue'
+    }}
+    >Messages</NavLink>
+  </div>;
+
+
+ReactDOM.render((
+  <Router>
     <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path='/signup' render={SignUp} />
+      <Route exact path='/signup/pl0x' render={() => <h1>OK fine, you l337 h4xx0r</h1>} />
+      <Route exact path='/messages' component={Messages} />
+  </div>
+  </Router>),
   document.getElementById('root')
 );
